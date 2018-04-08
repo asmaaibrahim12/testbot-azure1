84540b9e6cdf3968f7ae630a0663a6b04776fffa
asmaaibrahim12 <asmaa.ibrahim12@gmail.com>
2018-04-08 09:02:26 +0200

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=samplechatbot101-Web-Deploy.pubxml -p:Password=Wo1qo5Ew36r2sBsvxs92zFntaBpWRhts1vtFjTZq2wrWPNaimplSRl8ah3fe
+
